8e8aa2f558593a473e4469ab2e453e6c1793c9cb
sadanand8821 <54492931+sadanand8821@users.noreply.github.com>
2024-07-01 13:09:37 +0530

    Update CustomMetricsController.java

--- EmailProcessor/CustomMetricsController.java
+++ EmailProcessor/CustomMetricsController.java
@@ -1,14 +1,14 @@
 package org.example;
 
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.Counter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 @RestController
@@ -17,16 +17,21 @@ public class CustomMetricsController {
 
     @Autowired
     private MeterRegistry meterRegistry;
+    private static final Logger logger = Logger.getLogger(CustomMetricsController.class.getName());
 
     @GetMapping
     public Map<String, Double> getCustomMetrics() {
-        return meterRegistry.getMeters().stream()
-                .filter(meter -> meter.getId().getName().startsWith("method.timed"))
+        logger.info("Fetching custom metrics...");
+        Map<String, Double> metrics = meterRegistry.getMeters().stream()
+                .filter(meter -> meter.getId().getName().startsWith("method.counted"))
                 .collect(Collectors.toMap(
-                        meter -> meter.getId().getName(),
-                        meter -> meterRegistry.find(meter.getId().getName())
-                                .timer()
-                                .mean(TimeUnit.MILLISECONDS)
+                        meter -> meter.getId().getTag("method"),
+                        meter -> {
+                            Counter counter = meterRegistry.find(meter.getId().getName()).counter();
+                            return (counter != null) ? counter.count() : 0.0;
+                        }
                 ));
+        logger.info("Custom metrics: " + metrics);
+        return metrics;
     }
 }
