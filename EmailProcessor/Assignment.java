9486e446a3543dc74a805f96009a40dae280d3fc
sadanand8821 <54492931+sadanand8821@users.noreply.github.com>
2024-07-09 20:49:13 +0530

    Update Assignment.java

--- EmailProcessor/Assignment.java
+++ EmailProcessor/Assignment.java
@@ -15,3 +15,40 @@ if (redirects.length > 0) {
 } else {
     console.log("Using initial request URL");
 }
+
+
+
+// Log to confirm the test script is running
+console.log("Test script running");
+
+// Get redirects array from collection variables
+let redirects = pm.collectionVariables.get("redirects") ? JSON.parse(pm.collectionVariables.get("redirects")) : [];
+console.log("Current redirects:", redirects);
+
+// Log all response headers
+pm.response.headers.all().forEach(function(header) {
+    console.log(header.key + ": " + header.value);
+});
+
+// Check if the response has a 'Location' header
+if (pm.response.headers.has('Location')) {
+    // Push the Location header value to the redirects array
+    let locationHeader = pm.response.headers.get('Location');
+    console.log("Found Location header:", locationHeader);
+    redirects.push(locationHeader);
+    
+    // Update the collection variable with the new redirects array
+    pm.collectionVariables.set("redirects", JSON.stringify(redirects));
+    console.log("Updated redirects:", JSON.stringify(redirects));
+    
+    // Set the next request to run, which will use the new URL
+    postman.setNextRequest(pm.info.requestName);
+} else {
+    console.log("No Location header found in the response or final response received");
+    
+    // Clear redirects if this is the final response
+    pm.collectionVariables.unset("redirects");
+}
+
+// Log the response code
+console.log("Response code:", pm.response.code);
